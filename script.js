0b69c8228ede15430fb7e27114e89d9615081ef3
Aniket <aniketpn123@gmail.com>
2025-11-24 23:56:33 +0530

    logic added

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-const input = document.querySelector('input');
+let task= "";
+const buttons = document.querySelectorAll('button');
+const inputField=document.querySelector('input');
 
-input.addEventListener('keydown',(e)=>{
-   console.log(e.key);
-   
+buttons.forEach((button)=>{
+   button.addEventListener('click',(e)=>{
+      if(e.target.innerHTML == '='){
+         task = eval(task);
+         inputField.value=task;
+         return;
+      }
+
+      if(e.target.innerHTML == 'DEL'){
+         task= task.slice(0,-1);
+         inputField.value=task;
+         return;
+      }
+
+      if(e.target.innerHTML == 'AC'){
+         task="";
+         inputField.value=task;
+         return;
+      }
+      task += e.target.innerHTML;
+      inputField.value=task
+   })
 })
+
